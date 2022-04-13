d45e6e07353763862ee266a0ad504e837f28f514
Mateusz Paprocki <mattpap@gmail.com>
2022-04-13 12:59:49 +0200

    Update after changes to PlotView.renderer_views

--- bokehjs/test/unit/regressions.ts
+++ bokehjs/test/unit/regressions.ts
@@ -139,7 +139,7 @@ describe("Bug", () => {
 
         const {view} = await display(plot)
 
-        const lnv = view.renderer_views.get(renderer)!
+        const lnv = view.renderer_view(renderer)!
         const ln_spy = sinon.spy(lnv, "request_render")
         const ui = view.canvas.ui_event_bus
         const {left, top} = offset(ui.hit_area)
@@ -169,7 +169,7 @@ describe("Bug", () => {
 
       const {view} = await display(plot)
 
-      const gv = view.renderer_views.get(renderer)!
+      const gv = view.renderer_view(renderer)!
       const gv_spy = sinon.spy(gv, "request_render")
       const ui = view.canvas.ui_event_bus
       const {left, top} = offset(ui.hit_area)
